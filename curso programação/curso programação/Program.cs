a885b4d1b2a861c2ce217ac0f0918423faf03d64
Richardajfw <richardeducation509@gmail.com>
2026-02-21 16:41:00 -0300

    Add files via upload

---
+++ curso programação/curso programação/Program.cs
@@ -0,0 +1,21 @@
+﻿using curso_programação;
+using System.Security.Cryptography.X509Certificates;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        //Aula1 aula1 = new Aula1();
+
+        aula2 aula2 = new aula2();
+        aula2.valorpar();
+
+
+    }
+}
+
+       
+
+
+
+
---
+++ curso programação/curso programação/aula2.cs
@@ -0,0 +1,23 @@
+﻿
+namespace curso_programação
+{
+    public class aula2
+    {
+
+
+
+        public void valorpar()
+        {
+            int numero = 3;
+            int resultado = 0;
+
+            resultado = 3 % 2;
+
+            Console.WriteLine(resultado);
+
+
+
+
+
+
+        }
