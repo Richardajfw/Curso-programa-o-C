0caffa4406b1d00c773ea62400bcf8ecfee0e6be
Richardajfw <richardeducation509@gmail.com>
2026-02-21 16:57:45 -0300

    Add files via upload

---
+++ curso programação/Aula1.cs
@@ -0,0 +1,92 @@
+﻿using System.Net.NetworkInformation;
+
+namespace curso_programação
+{
+    public class Aula1
+    {
+        public void Soma()
+        {
+
+            int valor1;
+            int valor2;
+
+
+            Console.WriteLine("informe o valo1");
+            valor1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("informe o valor2");
+            valor2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"total{valor1 + valor2}");
+        }
+
+
+        public void multiplicar ()
+        {
+
+            int valor1;
+            int valor2;
+
+
+            Console.WriteLine("informe o primeiro valo1");
+             valor1 = Convert.ToInt32(Console.ReadLine);
+
+            Console.WriteLine("informe o segundo valo2");
+             valor2 = Convert.ToInt32(Console.ReadLine);
+
+            Console.WriteLine($"total {valor1 * valor2}");
+
+        }
+
+
+        public void subtrair()
+        {
+            int valor1;
+            int valor2;
+
+
+            Console.WriteLine("informe o primeiro valor1");
+            valor1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("informe o segundo valor2");
+            valor2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"total {valor1 - valor2}");
+
+
+                    }
+
+
+
+        public void divir() 
+        {
+            int valor1;
+            int valor2;
+
+            Console.WriteLine("informe o primeiro valor1");
+            valor1 = Convert.ToInt32(Console.Read);
+
+
+            Console.WriteLine("informe o segundo valor2");
+            valor2 = Convert.ToInt32(Console.Read);
+
+
+            Console.WriteLine($"total {valor1 / valor2}");
+
+          
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
---
+++ curso programação/aula2.cs
@@ -0,0 +1,52 @@
+﻿
+using System.ComponentModel.Design;
+
+namespace curso_programação
+{
+    public class aula2
+    {
+
+
+
+        public void valorpar() {
+
+          
+
+            Console.WriteLine("informe o valor");
+
+            int numero = int.Parse(Console.ReadLine());
+            if (numero % 2 == 0);
+
+
+            Console.WriteLine("PAR");
+
+
+            else
+                Console.WriteLine("IMPAR");
+        
+        
+        
+        
+        
+        
+        
+        }
+
+
+
+
+        
+        
+        
+        
+    
+
+
+
+
+         
+
+        
+
+
+        
